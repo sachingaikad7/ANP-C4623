424fe8b8911a3e219032eb1d7f1a0f62a152cc93
Sachin Prakash Gaikwad <124521561+sachingaikad7@users.noreply.github.com>
2023-06-07 23:05:23 +0530

    Add files via upload

---
+++ package-info.java
@@ -0,0 +1,24 @@
+package corejava;
+
+
+package InterfaceAnimals;
+
+interface Animalss 
+{
+public void sound();//Interface method() without body...
+}
+class Monkey implements Animalss//Creating class Monkey 
+{
+	public void sound()//Body of interface method sound
+	{
+		System.out.println("Monkey ......");
+	}
+}
+class Main1//Creating class with main method()
+{
+	public static void main(String[] args)
+	{
+		Monkey m= new Monkey();//Creating object of class Monkey
+		m.sound();//Calling method() of interface		
+	}
+}
