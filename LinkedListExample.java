fcb43d6f2e5c332cde2f52bd3f61f436535be2ea
Sachin Prakash Gaikwad <124521561+sachingaikad7@users.noreply.github.com>
2023-06-07 23:06:16 +0530

    Add files via upload

---
+++ LinkedListExample.java
@@ -0,0 +1,21 @@
+
+
+package packagecollection;
+import java.util.LinkedList;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        // create a linked list of integers
+        LinkedList<Integer> list = new LinkedList<Integer>();
+
+        // add elements to the linked list
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        // display the contents of the linked list
+        System.out.println("Linked List Contents: " + list);
+    }
+}
---
+++ TreeSetExample.java
@@ -0,0 +1,20 @@
+package packagecollection;
+
+import java.util.TreeSet;
+import java.util.Set;
+
+public class TreeSetExample 
+{
+
+	public static void main(String[] args)
+	{
+		Set<String> s=new TreeSet<String>();	
+		s.add("A");
+		s.add("B");
+		s.add("C");
+		s.add("C");
+		System.out.println("Elements are:" + s);
+
+	}
+
+}
